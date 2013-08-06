349814b2fcf8d70d2bbef62adaf211baf6c9cdb2
Immanuel Bayer <immanuel.bayer@uni-konstanz.de>
2013-08-06 20:20:28 +0200

    use correct gestrue to id convertion

--- gesture_modelling.py
+++ gesture_modelling.py
@@ -1,9 +1,16 @@
 from preprocessing import aggregated_skeletion
 from sklearn.ensemble import ExtraTreesClassifier
-from sklearn import preprocessing
 from sklearn.externals import joblib
 import pandas as pd
-from functools import partial
+import numpy as np
+
+
+
+gesture_to_id = {'vieni':2, 'break':0, 'prend':14, 'sonos':20, 'chevu':6,
+        'dacco':7, 'perfe':3, 'vatte':1, 'basta':13, 'buoni':18, 'chedu':5,
+        'cosat':12, 'fame':16, 'nonce':15, 'furbo':4, 'combi':9, 'frega':10,
+        'seipa':8, 'tanto':17, 'messi':19, 'ok':11}
+id_to_gesture = dict(zip(gesture_to_id.values(), gesture_to_id.keys()))
 
 
 def trained_rf(file_names=['training1', 'training2', 'training3',
@@ -14,17 +21,15 @@ def trained_rf(file_names=['training1', 'training2', 'training3',
         X, y = aggregated_skeletion(file_names=file_names,
                 agg_functions=['median', 'var'])
         X = X.fillna(0)
-
-        le = preprocessing.LabelEncoder()
-        y = le.fit_transform(y)
+        y = np.array([gesture_to_id[gest] for gest in y])
 
 
         clf = ExtraTreesClassifier(n_estimators=500, random_state=0,
             n_jobs=-1)
         clf.fit(X, y)
 
-        _ = joblib.dump((clf, le), filename, compress=9)
+        _ = joblib.dump(clf, filename, compress=9)
     else:
-        clf, le = joblib.load(filename)
+        clf = joblib.load(filename)
 
-    return clf, le
+    return clf
