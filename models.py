83dfc720d4467cee456ee7123a95af4f332d4813
Immanuel Bayer <immanuel.bayer@uni-konstanz.de>
2013-08-07 13:00:32 +0200

    fix import error

---
+++ models.py
@@ -0,0 +1,45 @@
+from gesture_modelling import gesture_to_id
+from gesture_modelling import dump_predictions
+from preprocessing import aggregated_skeletion
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.externals import joblib
+import pandas as pd
+from pandas import DataFrame
+import numpy as np
+
+
+def leaderboard_model(out_file='leaderboard.csv', retrain=False):
+
+    filename = 'cache/joblib/rf_leaderboard.joblib.pkl'
+    file_names=['training1', 'training2', 'training3',
+                                    'training4']
+    filename = 'cache/joblib/gesture_rf.joblib.pkl'
+
+    if retrain:
+        X, y = aggregated_skeletion(file_names=file_names,
+                agg_functions=['median', 'var', 'min', 'max'])
+        X = X.fillna(0)
+        y = np.array([gesture_to_id[gest] for gest in y])
+
+
+        clf = ExtraTreesClassifier(n_estimators=500, random_state=0,
+            n_jobs=-1)
+        clf.fit(X, y)
+        _ = joblib.dump(clf, filename, compress=9)
+    else:
+        clf = joblib.load(filename)
+
+    X_win = aggregated_skeletion_win(['validation1_lab', 'validation2_lab', 'validation3_lab'])
+
+    y_pred = clf.predict(X_win)
+    df_pred = DataFrame.from_records(X_win.index.tolist(), columns=['sample_id', 'frame'])
+    df_pred['gesture'] = y_pred
+
+    dump_predictions(df_pred,out_file , convert_to_id=False)
+
+    print 'done, predictions are in ' + out_file
+
+
+if __name__ == '__main__':
+
+    leaderboard_model(retrain=True)
