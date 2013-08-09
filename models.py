9e2cd58758cee9cb6c346f479afa23bf484286e6
Immanuel Bayer <immanuel.bayer@uni-konstanz.de>
2013-08-09 23:28:58 +0200

    leaderboard model basic + post-processing

--- models.py
+++ models.py
@@ -1,14 +1,16 @@
 from gesture_modelling import gesture_to_id
-from gesture_modelling import dump_predictions
-from preprocessing import aggregated_skeletion
-from preprocessing import aggregated_skeletion_win
+from postprocessing import postprocess, dump_predictions
+from preprocessing import aggregated_skeletion, aggregated_skeletion_win
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.externals import joblib
 import pandas as pd
 from pandas import DataFrame
 import numpy as np
+from joblib import Memory
+memory = Memory('cache/')
 
 
+@memory.cache
 def leaderboard_model(out_file='leaderboard.csv', retrain=False):
 
     filename = 'cache/joblib/rf_leaderboard.joblib.pkl'
@@ -29,18 +31,18 @@ def leaderboard_model(out_file='leaderboard.csv', retrain=False):
     else:
         clf = joblib.load(filename)
 
-    X_win = aggregated_skeletion_win(['validation1_lab', 'validation2_lab', 'validation3_lab'],
-            agg_functions=['median', 'var', 'min', 'max'])
+    X_win = aggregated_skeletion_win(['validation1_lab', 'validation2_lab',
+        'validation3_lab'], agg_functions=['median', 'var', 'min', 'max'])
 
-    y_pred = clf.predict(X_win)
-    df_pred = DataFrame.from_records(X_win.index.tolist(), columns=['sample_id', 'frame'])
-    df_pred['gesture'] = y_pred
+    y_pred = clf.predict_proba(X_win)
+    df_pred = DataFrame(y_pred, index=[s for (s, _) in X_win.index])
 
-    dump_predictions(df_pred,out_file , convert_to_id=False)
-
-    print 'done, predictions are in ' + out_file
+    to_dump = df_pred.groupby(level=0).apply(postprocess)
+    dump_predictions(to_dump, out_path=out_file)
+    return to_dump
 
 
 if __name__ == '__main__':
+    from models import leaderboard_model
 
     leaderboard_model(retrain=True)
