da32e81b9cc25de8b64d8c5d14d64b42502f25b1
Immanuel Bayer <immanuel.bayer@uni-konstanz.de>
2013-08-10 15:34:44 +0200

    add model to eval gesture acc

--- models.py
+++ models.py
@@ -42,7 +42,7 @@ def leaderboard_model(out_file='leaderboard.csv', retrain=False):
 
 
 @memory.cache
-def eval_model(out_file='eval_model.csv', retrain=False):
+def eval_seq_model(out_file='eval_model.csv', retrain=False):
 
     filename = 'cache/joblib/rf_eval_model.joblib.pkl'
     file_names=['training1', 'training3', 'training4', 
@@ -73,8 +73,38 @@ def eval_model(out_file='eval_model.csv', retrain=False):
     return to_dump
 
 
+@memory.cache
+def eval_gesture_model(retrain=False):
+
+    filename = 'cache/joblib/rf_eval_model.joblib.pkl'
+    file_names=['training1', 'training3', 'training4', 
+            'validation1_lab', 'validation3_lab']
+
+    if retrain:
+        X, y = aggregated_skeletion(file_names=file_names,
+                agg_functions=['median', 'var', 'min', 'max'])
+        X = X.fillna(0)
+        y = np.array([gesture_to_id[gest] for gest in y])
+
+
+        clf = ExtraTreesClassifier(n_estimators=500, random_state=0,
+            n_jobs=-1)
+        clf.fit(X, y)
+        _ = joblib.dump(clf, filename, compress=9)
+    else:
+        clf = joblib.load(filename)
+
+    X_test, y_test = aggregated_skeletion(['validation2_lab', 'training2'],
+            agg_functions=['median', 'var', 'min', 'max'])
+    X_test = X_test.fillna(0)
+    y_pred = clf.predict_proba(X_test)
+    return y_pred, y_test
+
+
 if __name__ == '__main__':
-    from models import leaderboard_model, eval_model
+    from models import leaderboard_model, eval_seq_model, eval_gesture_model
 
     #leaderboard_model(retrain=True)
-    eval_model(retrain=True)
+    #eval_seq_model(retrain=True)
+    eval_gesture_model()
+
