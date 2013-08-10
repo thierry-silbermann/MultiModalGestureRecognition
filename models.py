c00a2f70dd6613be749040b13c774c142b6f9776
Immanuel Bayer <immanuel.bayer@uni-konstanz.de>
2013-08-10 14:25:57 +0200

    add eval model predict train2 and valid2

--- models.py
+++ models.py
@@ -3,7 +3,6 @@ from postprocessing import postprocess, dump_predictions
 from preprocessing import aggregated_skeletion, aggregated_skeletion_win
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.externals import joblib
-import pandas as pd
 from pandas import DataFrame
 import numpy as np
 from joblib import Memory
@@ -42,7 +41,40 @@ def leaderboard_model(out_file='leaderboard.csv', retrain=False):
     return to_dump
 
 
-if __name__ == '__main__':
-    from models import leaderboard_model
+@memory.cache
+def eval_model(out_file='eval_model.csv', retrain=False):
 
-    leaderboard_model(retrain=True)
+    filename = 'cache/joblib/rf_eval_model.joblib.pkl'
+    file_names=['training1', 'training3', 'training4', 
+            'validation1_lab', 'validation3_lab']
+
+    if retrain:
+        X, y = aggregated_skeletion(file_names=file_names,
+                agg_functions=['median', 'var', 'min', 'max'])
+        X = X.fillna(0)
+        y = np.array([gesture_to_id[gest] for gest in y])
+
+
+        clf = ExtraTreesClassifier(n_estimators=500, random_state=0,
+            n_jobs=-1)
+        clf.fit(X, y)
+        _ = joblib.dump(clf, filename, compress=9)
+    else:
+        clf = joblib.load(filename)
+
+    X_win = aggregated_skeletion_win(['validation2_lab', 'training2'],
+            agg_functions=['median', 'var', 'min', 'max'])
+
+    y_pred = clf.predict_proba(X_win)
+    df_pred = DataFrame(y_pred, index=[s for (s, _) in X_win.index])
+
+    to_dump = df_pred.groupby(level=0).apply(postprocess)
+    dump_predictions(to_dump, out_path=out_file)
+    return to_dump
+
+
+if __name__ == '__main__':
+    from models import leaderboard_model, eval_model
+
+    #leaderboard_model(retrain=True)
+    eval_model(retrain=True)
