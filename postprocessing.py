3b275f2ae870f0a5cd73dac00fd89f1fc3b3dbca
Immanuel Bayer <immanuel.bayer@uni-konstanz.de>
2013-08-20 12:55:26 +0200

    add pad and smoothing to postprocessing

--- postprocessing.py
+++ postprocessing.py
@@ -104,3 +104,77 @@ def dump_predictions(df, out_path='pred.csv', convert_to_id=False):
 
             out = sample_id[-4:] + ',' + gestures + '\n'
             f.write(out)
+
+
+# source: SciPy-CookBook
+def smooth(x,window_len=11,window='hanning'):
+    """smooth the data using a window with requested size.
+
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+
+    input:
+        x: the input signal 
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+
+    see also: 
+
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+
+
+    if window_len<3:
+        return x
+
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    #return y
+    return y[(window_len/2 - (window_len + 1)%2):-(window_len/2)]
+
+
+def pad_smooth(sample, window_len):
+    nr_frames = sample.frame.max() + 40
+    out = DataFrame({'frame': np.arange(nr_frames)})
+    out['sample_id'] = sample.sample_id.unique()[0]
+
+    out = pd.merge(out, sample, how='outer', on=['sample_id', 'frame'])
+    out.fillna(method='ffill', inplace=True, limit=2)
+    out.fillna(method='bfill', inplace=True)
+    out.fillna(method='ffill', inplace=True)
+    for gesture in range(21):
+        out[gesture] = smooth(out[gesture], window_len=window_len, window='hanning')
+    return out
