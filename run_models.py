027c79b588c4a4426e3a83ad7092dab4b285c754
Thierry Silbermann <thierry.silbermann@gmail.com>
2013-08-20 15:17:26 +0200

    last version

--- run_models.py
+++ run_models.py
@@ -2,12 +2,36 @@ from models import movement_interval
 from pandas import DataFrame
 import pandas as pd
 import numpy as np
+import os
+from algo_multi_modal_v3 import *
 from postprocessing import pad_smooth
 
+def train_audio_models(train_on, predict_on, submission_table_filename, root):
+    
 
-def train_audio_models(train_on, predict_on):
-    pass
+    #Training part
+    wav_list = []
+    for directory in train_on: 
+        wav_list += getAllWav(directory, True, root)
+    wav_list.sort() #Just in case
 
+    print '=> Features creation and training on gestures: 20mn'
+    train_model_on_gestures(wav_list)
+    
+    print '=> Features creation and training on sound: 4h18'
+    train_model_on_sound(wav_list)
+    
+    #Predicting part
+    wav_list = []
+    for directory in predict_on: 
+        wav_list += getAllWav(directory, True, root)
+    wav_list.sort() #Just in case
+    
+    #wav_list = getOneWav(root, 'training1', 'Sample00001') # Uncomment this line and 
+                                                            # comment the previous three line to do prediction on one sample
+    
+    print '=> Full prediction: 41mn'
+    blend_model(wav_list, submission_table_filename)
 
 def train_movement_model_and_merge_on_audio_interval(train_on, predict_on,
         path_to_audio_intervals, path_to_movement_model_with_audio_interval):
@@ -24,40 +48,45 @@ def train_movement_model_and_merge_on_audio_interval(train_on, predict_on,
 
     middle_probs.to_csv(path_to_movement_model_with_audio_interval, index=False)
 
-
-def extract_audio_intervals_from_movement_model():
-    pass
-
-
-def merge_models():
-    pass
-
-
-def create_prediction_file():
-    pass
+def merge_models(path_to_audio_intervals, path_to_movement_model_with_audio_interval):
+    os.system("paste -d ',' " + path_to_audio_intervals + " " + path_to_movement_model_with_audio_interval + " > final_" + path_to_audio_intervals)
 
 
 if __name__ == '__main__':
 
+
+    root = '/home/thierrysilbermann/Documents/Kaggle/11_Multi_Modal_Gesture_Recognition'
+    path_to_audio_intervals = 'Submission_table.csv'
+    path_to_movement_model_with_audio_interval = 'middle_proba_added.csv'
+
     #leaderboard model settings
-    predict_on = ['validation1_lab', 'validation2_lab', 'validation3_lab']
     train_on = ['training1', 'training2', 'training3', 'training4']
+    predict_on = ['validation1_lab', 'validation2_lab', 'validation3_lab']
     path_to_audio_intervals = 'Submission_table_t1234_v123.csv'
+    
+    train_audio_models(train_on, predict_on, path_to_audio_intervals, root)
     path_to_movement_model_with_audio_interval =\
             'movement_probs_added_' + path_to_audio_intervals
 
     train_movement_model_and_merge_on_audio_interval(train_on, predict_on,
             path_to_audio_intervals, path_to_movement_model_with_audio_interval)
 
+    '''
     # parameter estimation settings
     predict_on = ['training2', 'validation2_lab']
     train_on = ['training1', 'training3', 'training4', 'validation1_lab', 'validation3_lab']
     path_to_audio_intervals = 'Submission_table_t134v13_t2v2.csv'
+    train_audio_models(train_on, predict_on, path_to_audio_intervals, root)
     path_to_movement_model_with_audio_interval =\
             'movement_probs_added_' + path_to_audio_intervals
+    '''
 
     train_movement_model_and_merge_on_audio_interval(train_on, predict_on,
             path_to_audio_intervals, path_to_movement_model_with_audio_interval)
+            
+    merge_models(path_to_audio_intervals, path_to_movement_model_with_audio_interval)
+    
+    submission('final_'+path_to_audio_intervals)
 
     # test set model
     #train_on = ['training1', 'training2', 'training3', 'training4', 
@@ -65,4 +94,3 @@ if __name__ == '__main__':
     #predict_on = ['test1', 'test2', 'test3', 'test4', 'test5', 'test6']
 
 
-    #train_audio_models(train_on, predict_on, path_to_audio_intervals)
