d52b1c1a29844ee53739fb9a273ba56fcde6182c
Thierry Silbermann <thierry.silbermann@gmail.com>
2013-08-20 15:12:06 +0200

    fuck

--- run_models.py
+++ run_models.py
@@ -3,6 +3,7 @@ from pandas import DataFrame
 import pandas as pd
 import numpy as np
 <<<<<<< HEAD
+<<<<<<< HEAD
 import os
 from algo_multi_modal_v3 import *
 
@@ -34,6 +35,8 @@ def train_audio_models(train_on, predict_on, submission_table_filename, root):
     print '=> Full prediction: 41mn'
     blend_model(wav_list, submission_table_filename)
 
+=======
+>>>>>>> fa875c7f7e5e3702e6e7c99263ff8a557a4cf7c2
 from postprocessing import pad_smooth
 
 
@@ -44,6 +47,20 @@ def train_audio_models(train_on, predict_on):
 def train_movement_model_and_merge_on_audio_interval(train_on, predict_on,
         path_to_audio_intervals, path_to_movement_model_with_audio_interval):
 
+<<<<<<< HEAD
+=======
+
+    df_out = movement_interval(train_on=train_on, predict_on=predict_on)
+    df_out = df_out.groupby('sample_id').apply(pad_smooth, window_len=11)
+
+    middle = pd.read_csv(path_to_audio_intervals, header=None, skiprows=1)
+    middle = middle.ix[:, [0, 2]]
+    middle.columns = ['sample_id', 'frame']
+    middle_probs = pd.merge(middle, df_out, how='left', on=['sample_id', 'frame'])
+    middle_probs = middle_probs.drop(['sample_id', 'frame', 0, 'movement'], axis=1)
+
+    middle_probs.to_csv(path_to_movement_model_with_audio_interval, index=False)
+>>>>>>> fa875c7f7e5e3702e6e7c99263ff8a557a4cf7c2
 
     df_out = movement_interval(train_on=train_on, predict_on=predict_on)
     df_out = df_out.groupby('sample_id').apply(pad_smooth, window_len=11)
@@ -73,6 +90,7 @@ if __name__ == '__main__':
     train_movement_model_and_merge_on_audio_interval(train_on, predict_on,
             path_to_audio_intervals, path_to_movement_model_with_audio_interval)
 
+<<<<<<< HEAD
     # parameter estimation settings
     predict_on = ['training2', 'validation2_lab']
     train_on = ['training1', 'training3', 'training4', 'validation1_lab', 'validation3_lab']
@@ -80,6 +98,24 @@ if __name__ == '__main__':
     path_to_movement_model_with_audio_interval =\
             'movement_probs_added_' + path_to_audio_intervals
 
+=======
+    #leaderboard model settings
+    predict_on = ['validation1_lab', 'validation2_lab', 'validation3_lab']
+    train_on = ['training1', 'training2', 'training3', 'training4']
+    path_to_audio_intervals = 'Submission_table_t1234_v123.csv'
+    path_to_movement_model_with_audio_interval =\
+            'movement_probs_added_' + path_to_audio_intervals
+
+    train_movement_model_and_merge_on_audio_interval(train_on, predict_on,
+            path_to_audio_intervals, path_to_movement_model_with_audio_interval)
+
+    # parameter estimation settings
+    predict_on = ['training2', 'validation2_lab']
+    train_on = ['training1', 'training3', 'training4', 'validation1_lab', 'validation3_lab']
+    path_to_audio_intervals = 'Submission_table_t134v13_t2v2.csv'
+    path_to_movement_model_with_audio_interval =\
+            'movement_probs_added_' + path_to_audio_intervals
+>>>>>>> fa875c7f7e5e3702e6e7c99263ff8a557a4cf7c2
 
     train_movement_model_and_merge_on_audio_interval(train_on, predict_on,
             path_to_audio_intervals, path_to_movement_model_with_audio_interval)
@@ -94,4 +130,13 @@ if __name__ == '__main__':
     #predict_on = ['test1', 'test2', 'test3', 'test4', 'test5', 'test6']
 
 
+<<<<<<< HEAD
+=======
+    # test set model
+    #train_on = ['training1', 'training2', 'training3', 'training4', 
+    #        'validation1_lab', 'validation2_lab', 'validation3_lab']
+    #predict_on = ['test1', 'test2', 'test3', 'test4', 'test5', 'test6']
+
+
+>>>>>>> fa875c7f7e5e3702e6e7c99263ff8a557a4cf7c2
     #train_audio_models(train_on, predict_on, path_to_audio_intervals)
