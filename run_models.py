54ae6fba57dea323683e755d447990af210e13ea
Immanuel Bayer <immanuel.bayer@uni-konstanz.de>
2013-08-21 14:22:56 +0200

    clean up run_models

--- run_models.py
+++ run_models.py
@@ -38,7 +38,7 @@ def train_movement_model_and_merge_on_audio_interval(train_on, predict_on,
 
 
     df_out = movement_interval(train_on=train_on, predict_on=predict_on)
-    df_out = df_out.groupby('sample_id').apply(pad_smooth, window_len=11)
+    df_out = df_out.groupby('sample_id').apply(pad_smooth, window_len=20)
 
     middle = pd.read_csv(path_to_audio_intervals, header=None, skiprows=1)
     middle = middle.ix[:, [0, 2]]
@@ -52,42 +52,53 @@ def merge_models(path_to_audio_intervals, path_to_movement_model_with_audio_inte
     os.system("paste -d ',' " + path_to_audio_intervals + " " + path_to_movement_model_with_audio_interval + " > final_" + path_to_audio_intervals)
 
 
-if __name__ == '__main__':
-
-
-    root = 'data/raw_data/' #/home/thierrysilbermann/Documents/Kaggle/11_Multi_Modal_Gesture_Recognition/
-    path_to_audio_intervals = 'Submission_table.csv'
-    path_to_movement_model_with_audio_interval = 'movement_probs_added_' + path_to_audio_intervals
-
-    #leaderboard model settings
-    train_on = ['training1', 'training2', 'training3', 'training4']
-    predict_on = ['validation1_lab', 'validation2_lab', 'validation3_lab']
-    
-    train_audio_models(train_on, predict_on, path_to_audio_intervals, root)
+def run_val1_model():
+    # parameter estimation settings
+    predict_on = ['training1', 'validation1_lab']
+    train_on = ['training2', 'training3', 'training4', 'validation2_lab', 'validation3_lab']
+    path_to_audio_intervals = 'Submission_table_t234v23_t1v1.csv'
+    #train_audio_models(train_on, predict_on, path_to_audio_intervals, root)
+    path_to_movement_model_with_audio_interval =\
+            'movement_probs_added_' + path_to_audio_intervals
 
     train_movement_model_and_merge_on_audio_interval(train_on, predict_on,
             path_to_audio_intervals, path_to_movement_model_with_audio_interval)
 
-    '''
+
+def run_vali2_model():
     # parameter estimation settings
     predict_on = ['training2', 'validation2_lab']
     train_on = ['training1', 'training3', 'training4', 'validation1_lab', 'validation3_lab']
     path_to_audio_intervals = 'Submission_table_t134v13_t2v2.csv'
-    train_audio_models(train_on, predict_on, path_to_audio_intervals, root)
+    #train_audio_models(train_on, predict_on, path_to_audio_intervals, root)
     path_to_movement_model_with_audio_interval =\
             'movement_probs_added_' + path_to_audio_intervals
-    
+
     train_movement_model_and_merge_on_audio_interval(train_on, predict_on,
             path_to_audio_intervals, path_to_movement_model_with_audio_interval)
-    '''     
-    
+
+
+def run_final_model():
+    root = 'data/raw_data/' #/home/thierrysilbermann/Documents/Kaggle/11_Multi_Modal_Gesture_Recognition/
+    path_to_audio_intervals = 'Submission_table.csv'
+    path_to_movement_model_with_audio_interval = 'movement_probs_added_' + path_to_audio_intervals
+
+    train_on = ['training1', 'training2', 'training3', 'training4', 
+            'validation1_lab', 'validation2_lab', 'validation3_lab']
+    predict_on = ['test1', 'test2', 'test3', 'test4', 'test5', 'test6']
+
+    train_audio_models(train_on, predict_on, path_to_audio_intervals, root)
+
+    train_movement_model_and_merge_on_audio_interval(train_on, predict_on,
+            path_to_audio_intervals, path_to_movement_model_with_audio_interval)
+
     merge_models(path_to_audio_intervals, path_to_movement_model_with_audio_interval)
-    
+
     submission('final_'+path_to_audio_intervals)
 
-    # test set model
-    #train_on = ['training1', 'training2', 'training3', 'training4', 
-    #        'validation1_lab', 'validation2_lab', 'validation3_lab']
-    #predict_on = ['test1', 'test2', 'test3', 'test4', 'test5', 'test6']
 
+if __name__ == '__main__':
 
+    #run_final_model()
+    run_val1_model()
+    run_vali2_model()
